5b064fe9f85bb191ef8dab3e5fe3e23c490b6699
Sdogaru <sdogaru@ucsd.edu>
2021-05-20 13:59:53 -0700

    fixed small bug in unit test

--- tests/summoner_v4_test.py
+++ tests/summoner_v4_test.py
@@ -12,13 +12,13 @@ class Test_summoner_v4(unittest.TestCase):
         if result == -1:
             assert False, "Response code != 200"
 
-        self.assertEqual(result,"JLzDAXZ89AGuYb1Std1IF_PJ5wqs1WORI4URcV6fbq7mEQY")
+        self.assertEqual(result,"oCwWLVEdQEKEEpo2NciVKqjjvRtgoGtFiw6L5OgMpe1q27Q")
 
     # invalid username case
     def test_bad_username_to_encryptedAccountID(self):
         sv4 = summoner_v4()
         result = sv4.username_to_encryptedAccountID(";;;;;;;;")
         self.assertEqual(result,-1)
-        
+
 if __name__ == '__main__':
     unittest.main()
