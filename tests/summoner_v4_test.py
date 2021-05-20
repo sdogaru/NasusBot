0f7fc4a56381d5449875e2013fdaa79c450e5766
Sdogaru <sdogaru@ucsd.edu>
2021-05-20 13:48:57 -0700

    Added unittests for username to encyrpted id

--- tests/summoner_v4_test.py
+++ tests/summoner_v4_test.py
@@ -4,6 +4,21 @@ sys.path.append('../')
 import unittest
 from src.api.summoner_v4 import summoner_v4
 
-summoner_v4 = summoner_v4()
-result = summoner_v4.username_to_encryptedAccountID("Šeby")
-print(result)
+class Test_summoner_v4(unittest.TestCase):
+    # username exists, correct functionality case
+    def test_username_to_encryptedAccountID(self):
+        sv4 = summoner_v4()
+        result = sv4.username_to_encryptedAccountID("Šeby")
+        if result == -1:
+            assert False, "Response code != 200"
+
+        self.assertEqual(result,"JLzDAXZ89AGuYb1Std1IF_PJ5wqs1WORI4URcV6fbq7mEQY")
+
+    # invalid username case
+    def test_bad_username_to_encryptedAccountID(self):
+        sv4 = summoner_v4()
+        result = sv4.username_to_encryptedAccountID(";;;;;;;;")
+        self.assertEqual(result,-1)
+        
+if __name__ == '__main__':
+    unittest.main()
