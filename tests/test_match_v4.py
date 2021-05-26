37e67f3233b587b4727311f1e3d65ef4490534f1
Sdogaru <sdogaru@ucsd.edu>
2021-05-25 17:58:24 -0700

    added more tests for match

--- tests/test_match_v4.py
+++ tests/test_match_v4.py
@@ -24,8 +24,17 @@ class Test_match_v4(unittest.TestCase):
         if result == -1:
             assert False, "Response code != 200"
 
-
         self.assertTrue(len(result['matches']) == 50)
+
+    # test call to get_match_list with specified champion (RIVEN)
+    def test_get_match_list_champion(self):
+        mv4 = Match_v4()
+        result = mv4.get_match_list("oCwWLVEdQEKEEpo2NciVKqjjvRtgoGtFiw6L5OgMpe1q27Q",championId=92)
+
+        if result == -1:
+            assert False, "Response code != 200"
+
+        self.assertTrue(len(result['matches']) == 2)
 
     def test_get_match(self):
         mv4 = Match_v4()
