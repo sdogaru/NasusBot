4b845ee3301d17d24229e789ca02e04319cdf53a
Sdogaru <sdogaru@ucsd.edu>
2021-12-17 00:47:02 -0800

    get matchlist v5

--- src/api/match_v5.py
+++ src/api/match_v5.py
@@ -7,5 +7,21 @@ class Match_v5(Api):
         self.endpoint_url = "match/v5/"
 
 
-m = Match_v5('AMERICAS')
-print(m)
+    def get_match_list(self,puuid,queue=None,start=None,count=None):
+        url_puuid = urllib.parse.quote(encryptedaccountID)
+        URL = self.base_url +self.endpoint_url+ "matchlists/by-puuid/"+ url_accountId  + "?"
+
+        if queue != None:
+            URL += "queue=" + str(queue) + "&"
+        if start != None:
+            URL += "start=" + str(beginIndex) + "&"
+        if count != None:
+            URL += "count=" + str(endIndex) + "&"
+
+        URL += "api_key=" + self.api_key
+
+        result = self.make_api_request(URL)
+        if result == -1:
+            return -1
+        else:
+            return result
